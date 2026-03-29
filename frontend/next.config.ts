01722443e14cb5966a2ff6ef16617ed6043f5e63
ssgetjobdone-byte <ssgetjobdone@gmail.com>
2026-03-30 00:16:56 +0530

    Create next.config.ts

---
+++ frontend/next.config.ts
@@ -0,0 +1,9 @@
+diff --git a/frontend/next.config.ts b/frontend/next.config.ts
+new file mode 100644
+index 0000000000000000000000000000000000000000..db26c8ea2704c78533e4be6290f386e43f41e02f
+--- /dev/null
++++ b/frontend/next.config.ts
+@@ -0,0 +1,3 @@
++import type { NextConfig } from 'next';
++const nextConfig: NextConfig = {};
++export default nextConfig;
