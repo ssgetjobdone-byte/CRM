dcf4a24a9526efe4445e93e747d1e930eed1bc1f
ssgetjobdone-byte <ssgetjobdone@gmail.com>
2026-03-30 00:03:27 +0530

    Create __init__.py

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,7 @@
+diff --git a/backend/app/models/__init__.py b/backend/app/models/__init__.py
+new file mode 100644
+index 0000000000000000000000000000000000000000..aed4fa323c2c8001593fdfdcd878a21718800167
+--- /dev/null
++++ b/backend/app/models/__init__.py
+@@ -0,0 +1 @@
++from .models import *
